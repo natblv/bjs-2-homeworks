40500e2679f997921ef3d12eed9dcdded015bfe4
Natalia Beliaeva <nbelyaev.nb@gmail.com>
2023-10-09 11:30:22 +0600

    Первое задание польностью готово

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,30 @@
 "use strict"
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+	let arr = [];
+	let x1;
+	let x2;
+	let d = b ** 2 - 4 * a * c;
+
+	if (d > 0) {
+		x1 = (-b + Math.sqrt(d)) / (2 * a);
+		x2 = (-b - Math.sqrt(d)) / (2 * a);
+		arr.push(x1, x2);
+	} else if (d === 0) {
+		x1 = -b / (2 * a);
+		arr.push(x1);
+	}
+	return arr;
 }
 
+console.log(solveEquation());
+
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+	let P = percent / 100 / 12;
+	let S = amount - contribution;
+	let payment = S * (P + (P / (((1 + P) ** countMonths) - 1)));
+	let sum = payment * countMonths;
+	let newSum = sum.toFixed(2);
+	return Number(newSum);
 }
+
+console.log(calculateTotalMortgage(10, 0, 50000, 12));
