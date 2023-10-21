0d094fe1b5fd638497eff4d55c35e2dc347c47e6
Natalia Beliaeva <nbelyaev.nb@gmail.com>
2023-10-21 16:41:21 +0600

    Второе задание полностью готово

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,24 +1,77 @@
 function getArrayParams(...arr) {
+  let min = Infinity;
+  let max = -Infinity;
+  let sum = 0;
 
-  return { min: min, max: max, avg: avg };
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) max = arr[i];
+    if (arr[i] < min) min = arr[i];
+    sum += arr[i];
+  }
+
+  let avg = Number((sum / arr.length).toFixed(2));
+
+  return {min: min, max: max, avg: avg};
 }
 
 function summElementsWorker(...arr) {
-
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum;
 }
 
 function differenceMaxMinWorker(...arr) {
+  let min = Infinity;
+  let max = -Infinity;
 
+  if (arr.length) {
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] > max) max = arr[i];
+      if (arr[i] < min) min = arr[i];
+    }
+    return max - min;
+  }
+  return 0;
 }
 
 function differenceEvenOddWorker(...arr) {
-
-}
+  let sumEvenElement = 0;
+  let sumOddElement = 0;
+  for (let i = 0; i < arr.length; i++) {
+      if (arr[i] % 2 === 0) {
+        sumEvenElement += arr[i];
+      } else {
+        sumOddElement += arr[i];
+      }
+    }
+    return sumEvenElement - sumOddElement;
+  }
 
 function averageEvenElementsWorker(...arr) {
-
+  let sumEvenElement = 0;
+  let countEvenElement = 0;
+  if (arr.length) {
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] % 2 === 0) {
+        sumEvenElement += arr[i];
+        countEvenElement += 1;
+      }
+    }
+    let average = sumEvenElement / countEvenElement;
+    return average;
+  }
+  return 0;
 }
-
-function makeWork (arrOfArr, func) {
-
+  
+function makeWork(arrOfArr, func) {
+  let maxWorkerResult = -Infinity;
+  for (let i = 0; i < arrOfArr.length; i++) {
+      const result = func(...arrOfArr[i]);
+      if (result > maxWorkerResult) {
+          maxWorkerResult = result;
+      }
+  }
+ return maxWorkerResult;
 }
